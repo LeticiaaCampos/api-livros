67dee33004f159f5014f0b8e276dfaabb67e8037
Leticia Antonini Campos Cordeiro Gomes <108307349+LeticiaaCampos@users.noreply.github.com>
2023-09-01 07:16:16 -0300

    passei as coisas para pasta

---
+++ api/src/index.js
@@ -0,0 +1,22 @@
+
+const express = require('express')
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+const app = express()
+const port = 3000
+
+
+app.get('/', async (req, res) => {
+    const livros = await prisma.livros.findMany()
+    res.json(livros)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+//visualizar o json
+//npx nodemon .\src\index.js
+//banco de dados
+//npx prisma studio
---
+++ site/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>oi</h1>
+    </>
+  )
+}
+
