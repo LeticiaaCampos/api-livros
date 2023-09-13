2a816ca42b2c76434a3ab5feb7eb6ec04005cf06
Leticia Antonini Campos Cordeiro Gomes <22100130@aluno.cotemig.com.br>
2023-09-13 05:33:58 -0300

    alteracao no card

--- site/src/components/card.jsx
+++ site/src/components/card.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
 
-export default function Card({  nome, img, autor, sinopse, genero, tipo }) {
-    return (
-      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-      <div className="md:flex">
-        <div className="md:flex-shrink-0">
-          <img
-            className="h-48 w-full object-cover md:h-full md:w-48"
-            src={img}
-            alt={nome}
-          />
+export default function Card({ nome, img, autor, sinopse, genero, tipo }) {
+  return (
+    <div class="max-w-sm w-full lg:max-w-full lg:flex">
+      <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('{img}')">
+      </div>
+      <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div class="mb-8">
+          <div class="text-gray-900 font-bold text-xl mb-2">{nome}</div>
+          <p class="text-gray-600 mb-2">Autor: {autor}</p>
+          <p class="text-gray-700 text-base">{sinopse}</p>
         </div>
-        <div className="p-8">
-          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-            {genero}
-          </div>
-          <div className="mt-2 text-xl font-semibold">{nome}</div>
-          <p className="mt-2 text-gray-500">{autor}</p>
-          <p className="mt-2 text-gray-500">{tipo}</p>
-          <p className="mt-4">{sinopse}</p>
+        <div class="px-6 pt-4 pb-2">
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{genero}</span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{tipo}</span>
         </div>
       </div>
     </div>
+
   );
 }
- 
+//https://v1.tailwindcss.com/components/cards
